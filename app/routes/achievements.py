b5a98460b0c74d0990a45418008dd43bfd956eb6
KauanHK <kauankaestner06@gmail.com>
2025-06-22 10:40:43 -0300

    achievements.py atualizado para receber as colunas reward_coins e xp_required

--- app/routes/achievements.py
+++ app/routes/achievements.py
@@ -1,5 +1,4 @@
 from flask import Blueprint, request, jsonify, abort
-from ..auth import token_required
 from ..db import db
 from ..models import Achievement
 from ..auth import admin_required
@@ -21,14 +20,15 @@ def index():
 @admin_required
 def create_achievement():
     data = request.get_json()
-    if 'name' not in data:
-        abort(400, description="Campo 'name' é obrigatório")
+    if 'name' not in data or 'xp_required' not in data:
+        abort(400, description="Campos 'name' e 'xp_required' são obrigatórios")
 
     if Achievement.query.filter_by(name=data['name']).first():
         abort(400, description="Já existe uma conquista com este nome")
 
     achievement = Achievement(
         name=data['name'],
+        xp_required=data['xp_required'],
         reward_coins=data.get('reward_coins', 0)
     )
 
@@ -47,14 +47,17 @@ def get_achievement(id: int):
 @bp.route('/<int:id>', methods = ['PUT'])
 @admin_required
 def update_achievement(id: int):
-    achievement = Achievement.query.get_or_404(id)
 
+    achievement = Achievement.query.get_or_404(id)
     data = request.get_json()
 
-    if 'name' in data:
+    if 'name' in data and data['name'] != achievement.name:
         if Achievement.query.filter_by(name=data['name']).first():
             abort(400, description="Já existe uma conquista com este nome")
         achievement.name = data['name']
+    
+    if 'xp_required' in data:
+        achievement.xp_required = data['xp_required']
 
     if 'reward_coins' in data:
         achievement.reward_coins = data['reward_coins']
